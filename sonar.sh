5a2c72f11e65545a13fa50056ab0a36b1012ea37
Yoichi KIKUCHI <yoichi.kikuchi.aug@gmail.com>
2016-02-09 14:55:43 +0900

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+PULLS_NUMBER=$1
+SONAR_USERNAME=$2
+SONAR_PASSWORD=$3
+GITHUB_OAUTH=$4
+
 echo "#####################"
 echo "#####################"
 echo $1
@@ -8,3 +13,18 @@ echo "#####################"
 
 
 mvn --version
+
+
+mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar  \
+-Dmaven.test.skip=true                        \
+-Dclirr=true                                  \
+-Dsonar.host.url=http://sonarqube.yo1000.com  \ 
+-Dsonar.login=${SONAR_USERNAME}               \ 
+-Dsonar.password=${SONAR_PASSWORD}            \
+-Dsonar.sourceEncoding=UTF-8                  \
+-Dsonar.analysis.mode=issues                  \
+-Dsonar.github.pullRequest=${PULLS_NUMBER}    \
+-Dsonar.github.oauth=${GITHUB_OAUTH}          \ 
+-Dsonar.github.repository=yo1000/fizzbuzz     \ 
+-B -e -V
+
